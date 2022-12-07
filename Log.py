1970c00ef30681b091959ea73242f69382539d7d
Jefferson Xavier <jejunior12@live.com>
2022-12-07 16:54:23 -0300

    Teste Log

--- Log.py
+++ Log.py
@@ -1,5 +1,17 @@
 import logging
+import os
+import datetime
+from datetime import date
 
-logging.basicConfig(level=logging.DEBUG, filename='Teste_Log.log', encoding='utf-8')
+MesAno = date.today()
+MesAno = MesAno.strftime('%m%Y')
 
-logging.info('Testando Log')
+Diretorio = f'C:\LogBot\{MesAno}'
+try:
+    os.makedirs(Diretorio)
+finally:
+    Dia = date.today()
+    Dia = Dia.strftime('%d')
+    Diretorio = f'{Diretorio}/{Dia}.log'
+    logging.basicConfig(level=logging.DEBUG, filename=Diretorio, encoding='utf-8')
+    logging.info('Testando Log')
