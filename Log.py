3ee2a8f870da5d36e1d65558275d4eaf95d7c43d
Jefferson Xavier <jejunior12@live.com>
2022-12-09 10:04:29 -0300

    Funcionando

--- Log.py
+++ Log.py
@@ -3,20 +3,23 @@ import os
 import datetime
 from datetime import date
 
-MesAno = date.today()
-MesAno = MesAno.strftime('%m%Y')
+def PreencheLog(Msg):
+    MesAno = date.today()
+    MesAno = MesAno.strftime('%m%Y')
 
-Diretorio = f'C:\LogBot\{MesAno}'
-try:
-    os.makedirs(Diretorio)
-finally:
-    Dia = date.today()
-    Dia = Dia.strftime('%d')
-    Diretorio = f'{Diretorio}/{Dia}.log'
+    Diretorio = f'C:\LogBot\{MesAno}'
+    try:
+        os.makedirs(Diretorio)
+    except:
+        exit
+    finally:
+        Dia = date.today()
+        Dia = Dia.strftime('%d')
+        Diretorio = f'{Diretorio}/{Dia}.log'
 
-    DataHora = datetime.datetime.now()
-    DataHora = DataHora.strftime('%d/%m/%Y %H:%M:%S')
+        DataHora = datetime.datetime.now()
+        DataHora = DataHora.strftime('%d/%m/%Y %H:%M:%S')
 
-    logging.basicConfig(format=f'{DataHora} - %(message)s', level=logging.INFO, filename=Diretorio, encoding='utf-8')
-    logging.info('Testando LOG')
-    os.system('cls')
+        logging.basicConfig(format=f'{DataHora} - %(message)s', level=logging.INFO, filename=Diretorio, encoding='utf-8')
+        logging.info(Msg)
+        os.system('cls')
