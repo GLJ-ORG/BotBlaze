5e51ff6c8eca3ca0053ca3786d5d3926e7dd87b3
luanmax.contato10@gmail.com <147852@luan>
2022-12-18 16:45:37 -0300

    Create funções.py

---
+++ funções.py
@@ -0,0 +1,33 @@
+
+
+def gales
+
+
+def branco
+
+
+def cor
+
+
+def simulador_banca
+
+
+def conta_real
+
+
+def gestao_banca
+
+
+def estrategia1
+
+
+def contadores_vd
+
+
+def inversao_numero
+
+
+def pegar_numeros
+
+
+def previsao_darodada
