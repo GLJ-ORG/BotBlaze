e794b6e1ac5f76e0397eb7fb2619d95e9eb88138
luanmax.contato10@gmail.com <147852@luan>
2022-12-25 12:18:46 -0300

    Create funcao.py

---
+++ funcao.py
@@ -0,0 +1,66 @@
+
+
+
+#def valor_entrada():
+ #   if contador_loss == 0
+  #      entradaP = gales()
+   #     guardar = gales() + branco
+
+#def gales():
+ #  if contador_loss == 0
+  #     gales = valor_entrada + branco
+
+
+#def branco:
+ #   Branco = entrada * 0.15
+  #  if valor_branco < 1.1
+   #     Branco = 1.1
+
+
+#def cor
+
+
+#def simulador_banca
+
+
+#def conta_real
+
+
+#def gestao_banca
+
+
+#def estrategia1
+
+
+#def contadores_vd
+    #if previsao != num2
+
+
+#def inversao_numero
+
+
+#def pegar_numeros
+
+
+#def previsao_darodada
+
+#def Cor_rodada():
+ #   if num1 == 1:
+  #      text_cor = 'Vermelho'
+   #         return text_cor
+    #elif num1 == 2:
+     #   text_cor = 'Preto'
+      #      return text_cor
+    #else:
+     #  text_cor = 'Coringa'
+      #      return text_cor
+
+def Cor_rodada(num1):
+  if num1 == 1:
+    return "vermelho"
+  elif num1 == 2:
+    return "preto"
+  elif num1 == 0:
+    return "coringa"
+  else:
+    return "cor inválida"
