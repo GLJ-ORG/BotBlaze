b5e66204ffb333734e5460acf8dc099a6d1d7af2
luanmax.contato10@gmail.com <147852@luan>
2023-01-02 20:46:24 -0300

    Update rewiw.py

--- rewiw.py
+++ rewiw.py
@@ -47,6 +47,8 @@ stop_gain = 0 #int(input('Qual sua meta de vitória em porcentagem(%)?'))
 quantidade_gales = 3 #int(input('Quantos gales você quer?'))
 multiplicador = 2 #int(input('Qual será seu fator multiplicador de gale?'))
 
+banca = 100
+gale = 0
 
 while 'true':
 # Receber dados da api OK
@@ -117,7 +119,7 @@ while 'true':
             contador_loss += 1
             zerador_loss += 1
 
-        if contador_loss == 3:
+        if contador_loss == 4:
             loss += 1
             contador_loss = 0
             zerador_loss = 0
@@ -139,6 +141,45 @@ while 'true':
         tempo_trabalho = total_partidas * 0.3 / 0.6
         #return Tempo_trabalhado
 
+    #Simulador de banca
+
+        valor_entrada = banca * 0.01
+        if valor_entrada < 1.1:
+            valor_entrada = 1.1
+
+        branco = valor_entrada * 0.15
+        if branco < 1.1:
+            branco = 1.1
+
+        g = valor_entrada + (branco * 2)
+        g1 = (g * 2) + branco
+        g2 = (g1 * 2) + branco
+        g3 = (g2 * 2) + branco
+        g4 = (g3 * 2) + branco
+        g5 = (g4 * 2) + branco
+
+        if contador_loss == 0:
+            gale = g
+        elif contador_loss == 1:
+            gale = g1
+        elif contador_loss == 2:
+            gale = g2
+        elif contador_loss == 3:
+            gale = g3
+        elif contador_loss == 4:
+            gale = g4
+        elif contador_loss == 5:
+            gale = g5
+
+        if num1 == prev2 & prev2 > 0:
+            banca += gale * 2
+        elif num1 == 0:
+            banca += branco * 14
+        else:
+            banca -= gale
+
+
+
 
         print(f'Gain: {gain} Loss: {loss} Coringa: {coringa}')
 
@@ -151,6 +192,8 @@ while 'true':
         Log.PreencheLog(str(f'Partida Nº{total_partidas} D|H Atual:{resultado_divisao}'))
 
         print(f'Tempo de trabalho: {tempo_trabalho} mim')
+
+        print(banca)
 
     # print dos dados para análise
         print(num1, prev2, lista_previsoes)
